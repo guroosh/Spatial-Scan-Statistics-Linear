7e241d9c33b4d139c7014ea1b8d81531777f9683
Lakshay dahiya <lakshay14057@iiitd.ac.in>
2018-07-29 17:15:02 +0530

    created format, coding of hj specific functions left

--- src/com/Executor/Main.java
+++ src/com/Executor/Main.java
@@ -390,6 +390,65 @@ public class Main {
 
 
     public static void main(String args[]) throws IOException {
+//        serial_execute();
+        habenero_execute();
+    }
+
+    private static void habenero_execute() {
+        //nodes ending on 21075
+        //activities starting on 21100
+        int new_nodes_starting_index = 21100;
+        // max_coor_x = -math.inf
+        // max_coor_y = -math.inf
+        // min_coor_x = math.inf
+        // min_coor_y = math.inf
+
+        int max_coor_x = -116;
+        int max_coor_y = 36;
+        int min_coor_x = -117;
+        int min_coor_y = 35;
+//
+//        int max_coor_x = 3;
+//        int max_coor_y = 3;
+//        int min_coor_x = -1;
+//        int min_coor_y = -1;
+
+        Files.readingNodeFile(min_coor_x, min_coor_y, max_coor_x, max_coor_y);
+
+        System.out.println(x.size());
+        num_n = x.size();
+        distances = new double[num_n][num_n];
+        paths = new Path[num_n][num_n];
+
+        //reading edge file into edges, only if both nodes are loaded
+        Files.readingEdgeFile();
+
+        System.out.println(edges.size());
+
+        startingDijkstra();
+
+        generatingActivitiesForEachEdge(new_nodes_starting_index);
+
+        num_a = activities.size();
+
+        activity_path_matrix = new Path[num_a][num_a];
+        matrix2 = new double[num_a][num_a];
+        number_of_activities_between_activities = new int[num_a][num_a];
+        final_answer = new double[num_a][num_a];
+
+        addingDistanceAndPathsBetweenActivities();
+        addingNumberOfActivitiesBetweenNodes();
+        addingNumberOfActivitiesBetweenActivities();
+//        printingEveryThing(matrix_, activities, edges, number_of_activities_between_activities, node_to_edges_map, matrix2, activity_path_matrix);
+        addingFinalAnswerMatrix();
+        try {
+            calculatingTheBestAnswerAndPlottingIt();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void serial_execute() {
 
         //nodes ending on 21075
         //activities starting on 21100
@@ -437,7 +496,11 @@ public class Main {
         addingNumberOfActivitiesBetweenActivities();
 //        printingEveryThing(matrix_, activities, edges, number_of_activities_between_activities, node_to_edges_map, matrix2, activity_path_matrix);
         addingFinalAnswerMatrix();
-        calculatingTheBestAnswerAndPlottingIt();
+        try {
+            calculatingTheBestAnswerAndPlottingIt();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
